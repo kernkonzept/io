2f7352df9a9c299aec1d6dbcde2d7e49a536dcf2
Matthias Lange <matthias.lange@kernkonzept.com>
2017-03-06 00:00:00 +0000

    io: add another PCI ID for i915 GPU handling
    On a Haswell i5-4300M the i915 GPU has the PCI ID 8086:0416.
    
    Change-Id: I56ff92708ee28ff02e4f10dfcc6bb7afb84f146d

--- io/server/src/drivers/pci/intel_i915.cc
+++ io/server/src/drivers/pci/intel_i915.cc
@@ -52,6 +52,7 @@ struct Init
     _pci_intel_i915_drv.register_driver(0x8086, 0x0046);
     _pci_intel_i915_drv.register_driver(0x8086, 0x0166);
     _pci_intel_i915_drv.register_driver(0x8086, 0x0412);
+    _pci_intel_i915_drv.register_driver(0x8086, 0x0416);
     _pci_intel_i915_drv.register_driver(0x8086, 0x1912);
   }
 };
