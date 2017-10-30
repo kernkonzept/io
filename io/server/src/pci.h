444cddb770ed7ed66cb1afecca4124f10e6bc974
Alexander Warg <alexander.warg@kernkonzept.com>
2017-10-30 00:00:00 +0000

    Irq_router_res: Allow to pass argument to ctor
    Add a template constructor that passes all arguments to the constructor
    of the encapsulated resource space. This shall allow us to initialize
    the encapsulated resource space directly within its constructor instead
    of add extra setup functions.
    
    Change-Id: Ib559f65ef6797ef370e15e0bb949c5609bd11207

--- io/server/src/pci.h
+++ io/server/src/pci.h
@@ -12,6 +12,7 @@
 #include "hw_device.h"
 #include <l4/sys/cxx/types>
 #include <l4/cxx/bitfield>
+#include <l4/cxx/type_traits>
 #include "irqs.h"
 
 #include <pthread.h>
@@ -688,6 +689,9 @@ protected:
   mutable Irq_rs _rs;
 
 public:
+  template< typename ...ARGS >
+  Irq_router_res(ARGS && ...args) : _rs(cxx::forward<ARGS>(args)...) {}
+
   RES_SPACE *provided() const { return &_rs; }
 };
 
