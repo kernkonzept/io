9d82b306a28ccc2091fc3f6daa491c40b7555ba2
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2019-04-22 00:00:00 +0000

    Fix bogus documentation of out parameter
    The device() method and the l4vbus_get_device function return
    information about a device. They expect a pointer to a structure where
    the device information shall be put after a successful call.
    
    This parameter is optional and if NULL is passed no information is
    returned. The documentation for the devinfo parameter suggested that
    the pointer may be set to NULL by the called function which is wrong.
    This corrects the documentation.
    
    Change-Id: I271a67edf0d2962762db6e30d7b5a092f34a0c11

--- libvbus/include/vbus.h
+++ libvbus/include/vbus.h
@@ -78,8 +78,7 @@ l4vbus_get_next_device(l4_cap_idx_t vbus, l4vbus_device_handle_t parent,
  * \param dev           Device handle of the device from which to retrieve the
  *                      details.
  * \param[out] devinfo  Information structure which contains details about
- *                      the device. The pointer might be NULL after a
- *                      successfull call.
+ *                      the device. The pointer might be NULL.
  *
  * \retval 0           Success.
  * \retval -L4_ENODEV  No device with the given device handle `dev` could be
