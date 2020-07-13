d70692b9303ee8c9c1c804929e7360d1f05864c7
Alexander Warg <alexander.warg@kernkonzept.com>
2020-07-13 00:00:00 +0000

    io: Add PCI SR-IOV and ARI capabilities
    This change adds definitions for PCI SR-IOV and PCI ARI capability
    structures.
    
    Change-Id: I62cfc56458ab2b33bf85caacafd17e1796dec278

--- io/server/src/pci-caps.h
+++ io/server/src/pci-caps.h
@@ -64,5 +64,62 @@ struct Pm_cap : Capability
   };
 };
 
+struct Sr_iov_cap : Capability
+{
+  enum { Id = 0x10 };
+
+  struct Caps : R<4, l4_uint32_t>
+  {
+    CXX_BITFIELD_MEMBER( 0,  0, vf_migration, v);
+    CXX_BITFIELD_MEMBER( 1,  1, ari_preserved, v);
+    CXX_BITFIELD_MEMBER(21, 31, vf_migration_msg, v);
+  };
+
+  struct Ctrl : R<8, l4_uint16_t>
+  {
+    CXX_BITFIELD_MEMBER( 0,  0, vf_enable, v);
+    CXX_BITFIELD_MEMBER( 1,  1, vf_migration_enable, v);
+    CXX_BITFIELD_MEMBER( 2,  2, vf_migration_irq_enable, v);
+    CXX_BITFIELD_MEMBER( 3,  3, vf_memory_enable, v);
+    CXX_BITFIELD_MEMBER( 4,  4, ari_capable_hierarchy, v);
+  };
+
+  struct Status : R<0xA, l4_uint16_t>
+  {
+    CXX_BITFIELD_MEMBER( 0,  0, vf_migartion_status, v);
+  };
+
+  using Initial_vfs  = R<0x0C, l4_uint16_t>;
+  using Total_vfs    = R<0x0E, l4_uint16_t>;
+  using Num_vfs      = R<0x10, l4_uint16_t>;
+  using Fn_dep       = R<0x12, l4_uint8_t>;
+  using Vf_offset    = R<0x14, l4_uint16_t>;
+  using Vf_stride    = R<0x16, l4_uint16_t>;
+  using Vf_device_id = R<0x1A, l4_uint16_t>;
+  using Supported_ps = R<0x1C, l4_uint32_t>;
+  using System_ps    = R<0x20, l4_uint32_t>;
+  using Vf_bar0      = R<0x24, l4_uint32_t>;
+  using Vf_migration_state = R<0x3C, l4_uint32_t>;
+};
+
+struct Ari_cap : Capability
+{
+  enum { Id = 0x0e };
+
+  struct Caps : R<0x04, l4_uint16_t>
+  {
+    CXX_BITFIELD_MEMBER( 0,  0, mfvc_func_groups, v);
+    CXX_BITFIELD_MEMBER( 1,  1, acs_func_groups, v);
+    CXX_BITFIELD_MEMBER( 8, 15, next_func, v);
+  };
+
+  struct Ctrl : R<0x06, l4_uint16_t>
+  {
+    CXX_BITFIELD_MEMBER( 0,  0, mfvc_func_groups, v);
+    CXX_BITFIELD_MEMBER( 1,  1, acs_func_groups, v);
+    CXX_BITFIELD_MEMBER( 4,  6, group, v);
+  };
+};
+
 } }
 
