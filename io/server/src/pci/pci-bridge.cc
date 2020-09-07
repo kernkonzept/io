4070d28092628e3f856ce1fd741bd427e8c7d2c8
Matthias Lange <matthias.lange@kernkonzept.com>
2020-09-07 00:00:00 +0000

    Revert "io: fix 'client' in request_child_resource() call"
    This reverts commit bb9b3109f7758e0a1148849cfa96abc21118e5f0.
    
    This seems to break IRQ routing for more complicated PCI bus structures
    with multiple PCI bridge levels.
    
    Change-Id: I4bbf5c198c02595768ffd0e7e5c14deea4cc4fda

--- io/server/src/pci/pci-bridge.cc
+++ io/server/src/pci/pci-bridge.cc
@@ -39,7 +39,7 @@ Pci_pci_bridge_irq_router_rs::request(Resource *parent, ::Device *pdev,
   if (pdev->parent())
     {
       child->start((child->start() + (cd->adr() >> 16)) & 3);
-      res = pdev->parent()->request_child_resource(child, cdev);
+      res = pdev->parent()->request_child_resource(child, pdev);
       if (res)
         child->parent(parent);
     }
