785a3fa688144ad7e122f8c13659f80abf176906
Andreas Otto <andreas.otto@kernkonzept.com>
2022-04-04 00:00:00 +0000

    libvbus: improve documention of PM interface
    Change-Id: I2e63c593b65dfddf460d4f6d261caf47fe26c624

--- libvbus/include/vbus_pm.h
+++ libvbus/include/vbus_pm.h
@@ -12,12 +12,32 @@
 #include <l4/vbus/vbus_types.h>
 #include <l4/sys/types.h>
 
+/**
+ * \ingroup l4vbus_module
+ * \defgroup l4vbus_pm_module L4vbus power management functions
+ * \{
+ */
+
 __BEGIN_DECLS
 
+/**
+ * \copybrief L4vbus::Pm::pm_suspend()
+ * \param vbus      V-BUS capability
+ * \param handle    Handle for the device to be suspended
+ * \copydetails L4vbus::Pm::pm_suspend()
+ */
 int L4_CV
 l4vbus_pm_suspend(l4_cap_idx_t vbus, l4vbus_device_handle_t handle);
 
+/**
+ * \copybrief L4vbus::Pm::pm_resume()
+ * \param vbus      V-BUS capability
+ * \param handle    Handle for the device to be resumed
+ * \copydetails L4vbus::Pm::pm_resume()
+ */
 int L4_CV
 l4vbus_pm_resume(l4_cap_idx_t vbus, l4vbus_device_handle_t handle);
+
+/**\}*/
 
 __END_DECLS
