22727c5d2a58a2415251599b289784fb825db2bf
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2024-01-15 00:00:00 +0000

    i915: check OpRegion Header for size
    The OpRegion header contains a size field, which tells about the size of
    the region to be mapped. Per default this size is 8k, however, this
    might be changed. Dynamically detect the size and request the resource.
    
    Change-Id: I804d692b71aafa66dc272153abb0ae1a71b3d24e

--- io/server/src/drivers/pci/intel_i915.cc
+++ io/server/src/drivers/pci/intel_i915.cc
@@ -15,9 +15,44 @@ using namespace Hw::Pci;
 
 struct Pci_intel_i915_drv : Driver
 {
+  struct Opregion_header
+  {
+    char const sign[16];  ///< Signature: "IntelGraphicsMem"
+    l4_uint32_t size;     ///< Size of OpRegion incl header in KB
+    l4_uint32_t over;     ///< OpRegion version
+    l4_uint32_t sver[8];  ///< System BIOS version
+    l4_uint32_t vver[4];  ///< Video BIOS version
+    l4_uint32_t gver[4];  ///< Graphics driver version
+    l4_uint32_t mbox;     ///< Supported mailboxes
+    l4_uint32_t dmod;     ///< Driver model
+    l4_uint32_t pcon;     ///< Platform configuration
+    l4_uint32_t dver[8];  ///< GOP/Driver version
+    l4_uint32_t rm01[31]; ///< Reserved must be zero
+  } __attribute__((packed));
+  static_assert(sizeof(Opregion_header) == 256, "Wrong OpRegion header size.");
+
+  /// Validate the signature of the OpRegion.
+  static bool check_opregion_sig(char const *sig)
+  { return 0 == memcmp(sig, "IntelGraphicsMem", 16); }
+
+  /// Map the OpRegion and check the signature; return nullptr on failure.
+  Opregion_header *map_opregion(l4_addr_t addr)
+  {
+    l4_addr_t base = res_map_iomem(addr, L4_PAGESIZE);
+    if (!base)
+      return nullptr;
+
+    Opregion_header *hdr = reinterpret_cast<Opregion_header *>(base);
+
+    if (!check_opregion_sig(hdr->sign))
+      return nullptr;
+
+    return hdr;
+  }
+
   int probe(Dev *d) override
   {
-    d_printf(DBG_DEBUG, "Found Intel i915 device\n");
+    d_printf(DBG_INFO, "Found Intel i915 device\n");
 
     l4_uint32_t v;
 
@@ -30,14 +65,29 @@ struct Pci_intel_i915_drv : Driver
     if (v == 0 || v == ~0U)
       return 0;
 
-    printf("Found Intel i915 GPU opregion at %x\n", v);
+    d_printf(DBG_DEBUG, "Found Intel i915 GPU OpRegion at %x\n", v);
+
+    Opregion_header *hdr = map_opregion(v);
+    l4_uint32_t size = 0U;
+    if (hdr)
+      {
+        size = hdr->size;
+        d_printf(DBG_DEBUG, "i915 OpRegion: size 0x%x, version 0x%x\n", size,
+                 hdr->over);
+      }
+    else
+      {
+        d_printf(DBG_WARN,
+                 "i915: OpRegion header invalid. Probing failed.\n");
+        return 0;
+      }
 
     unsigned flags =   Resource::Mmio_res | Resource::Mem_type_rw
                      | Resource::F_prefetchable;
 
     // the opregion address might not be page aligned... (0xdaf68018)
     Resource *res = new Resource(flags, l4_trunc_page(v),
-                                 l4_round_page(v + 0x2000) - 1);
+                                 l4_round_page(v + size * 0x400) - 1);
     d->host()->add_resource(res);
     Device *p;
     for (p = d->host()->parent(); p && p->parent(); p = p->parent())
